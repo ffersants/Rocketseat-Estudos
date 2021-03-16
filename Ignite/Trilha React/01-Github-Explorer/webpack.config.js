561533867822191506af118d55623b94071b4254
Fernando Ferreira <fersants.nando.ferreira@gmail.com>
2021-03-15 22:38:01 -0300

    HMR habilitado e configurado com React refresh webpack pluging.

--- Ignite/Trilha React/01-Github-Explorer/webpack.config.js
+++ Ignite/Trilha React/01-Github-Explorer/webpack.config.js
@@ -1,9 +1,13 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+const isDevelopment = process.env.NODE_ENV !== 'production'
 
-const isDevelopment = process.env.NODE_ENV === 'production'
+console.log(isDevelopment)
 
 module.exports = {
+    mode: isDevelopment ? 'development' : 'production',
+    devtool: isDevelopment ? 'eval-source-map' : 'source-map',
     //especifica o arquivo principal da aplicação
     entry: path.resolve(__dirname, 'src', 'Index.jsx'),
     output: {
@@ -13,12 +17,33 @@ module.exports = {
     resolve: {
         extensions: ['.jsx', '.js']
     },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public'),
+        hot: true
+    },    
+    plugins: [
+        //caso não estejamos em desenvolvimento, a verificação abaixo retornará
+        //false, mas false não é um plugin do webpack, logo dará um erro
+        isDevelopment && new ReactRefreshWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html')
+        })
+    //pelo motivo descrito acima, nós usamos a função filter, 
+    //que deixará no array de plugins somente aquilo que retornar true
+    ].filter(Boolean),
     module: {
         rules: [
             {
                 test: /\.jsx$/,
                 exclude: /node_modules/,
-                use: 'babel-loader'
+                use: { 
+                    loader: require.resolve('babel-loader'),
+                    options: {
+                        plugins: [
+                            isDevelopment && require.resolve('react-refresh/babel')
+                        ].filter(Boolean)
+                    }
+                }
             },
             {
                 test: /\.scss$/,
@@ -27,14 +52,4 @@ module.exports = {
             }
         ]
     },
-    mode: isDevelopment ? 'development' : 'production',
-    devServer: {
-        contentBase: path.resolve(__dirname, 'public')
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'public', 'index.html')
-        })
-    ],
-    devtool: isDevelopment ? 'eval-source-map' : 'source-map'
 }
