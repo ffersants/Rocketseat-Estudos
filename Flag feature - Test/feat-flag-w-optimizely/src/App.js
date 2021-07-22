55aad04dd40f56589b5f8f29ccd06f3475d9a7d4
Fernando Ferreira <fersants.nando.ferreira@gmail.com>
2021-07-22 00:38:45 -0300

    O trem funciona mesmo viu.

--- Flag feature - Test/feat-flag-w-optimizely/src/App.js
+++ Flag feature - Test/feat-flag-w-optimizely/src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import {
+  createInstance,
+  OptimizelyFeature,
+  OptimizelyProvider,
+} from "@optimizely/react-sdk";
+
+const optimizely = createInstance({
+  sdkKey: "E4DCQzudYfySnva1ZYuEz",
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <OptimizelyProvider
+      optimizely={optimizely}
+      user={{
+        id: "user123",
+      }}
+    >
+      <div className="App">
+        <header className="App-header">
+          <OptimizelyFeature feature="mudar_imagem_do_index">
+            {(enabled) =>
+              enabled ? (
+                <img src={logo} className="App-logo" alt="logo" />
+              ) : (
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3WaP8XZ0Z8Yw8jTuX7PD736bzDovzhXe7ZEFOCX4YecPAcADXOxVnWM9llXp3xHQ9vzU&usqp=CAU"
+                  className="App-logo"
+                  alt="logo"
+                />
+              )
+            }
+          </OptimizelyFeature>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </OptimizelyProvider>
   );
 }
 
