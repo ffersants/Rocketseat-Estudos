b78155557ca663cc03c68fb1d27163d4db649063
Fernando Ferreira <fersants.nando.ferreira@gmail.com>
2021-07-23 00:32:31 -0300

    Parou em 1:19. Tratar persistência do login on refresh com useEffect.

--- RocketSeat/NLW/NLW6/letmeask/src/App.tsx
+++ RocketSeat/NLW/NLW6/letmeask/src/App.tsx
@@ -1,11 +1,60 @@
-import {Button} from './Button'
+import { createContext, useState } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import {auth, firebase} from "./services/firebase"
+
+import { Home } from './pages/Home'
+import { NewRoom } from './pages/NewRoom'
+
+type User = {
+  id: string;
+  name: string;
+  avatar: string;
+}
+
+type AuthContextType = {
+  user: User | undefined;
+  signInWithGoogle: () => Promise<void>;
+}
+
+export const AuthContext = createContext({} as AuthContextType)
+
 function App() {
+  const [user, setUser] = useState<User>();
+
+  async function signInWithGoogle(){
+    const provider = new firebase.auth.GoogleAuthProvider() 
+
+    const result = await auth.signInWithPopup(provider)
+  
+    if(result.user){
+
+        const {displayName, photoURL, uid} = result.user
+
+        if(!displayName || !photoURL){
+          throw new Error("Missing info from Google Account.")
+        }
+
+        setUser({
+          id: uid,
+          name: displayName,
+          avatar: photoURL
+        })
+
+      }
+    
+
+  }
   return (
     <>
-      <h1>Hello world!</h1>
-      <Button>
-        Content passed as children!
-      </Button>
+      <BrowserRouter>
+        <AuthContext.Provider value={{user, signInWithGoogle}}>
+
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+
+        </AuthContext.Provider>
+      </BrowserRouter>
     </>
   );
 }
---
+++ RocketSeat/NLW/NLW6/letmeask/src/components/Button.tsx
@@ -0,0 +1,16 @@
+//importa os atributos HTML para button
+import { ButtonHTMLAttributes } from "react"
+
+import "../styles/button.scss"
+
+//atribui a tipagem de buttons para o elemento HTMLButtonElement
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+
+//passa essa tipagem para props, determinando o tipo de valor esperado
+//para cada atributo
+export function Button(props: ButtonProps){
+    return(
+        //desestruturação do objeto em chave="valor" || atributo="valor"
+        <button className="button" {...props}/>
+    )
+}
