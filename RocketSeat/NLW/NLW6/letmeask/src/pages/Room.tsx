54afe27e73f53d97445337a581258819f81a0438
Fernando Ferreira <fersants.nando.ferreira@gmail.com>
2021-07-30 02:50:34 -0300

    update.

--- RocketSeat/NLW/NLW6/letmeask/src/pages/Room.tsx
+++ RocketSeat/NLW/NLW6/letmeask/src/pages/Room.tsx
@@ -1,40 +1,131 @@
 import { useParams } from "react-router-dom";
 
+import { useAuth } from "../hooks/useAuth";
+
 import logoImg from "../assets/logo.svg";
 
 import { Button } from "../components/Button";
 import { RoomCode } from "../components/RoomCode";
 
 import "../styles/room.scss";
+import { database } from "../services/firebase";
+import { FormEvent, useEffect, useState } from "react";
 
 type RoomParams = {
   id: string;
 };
 
+type FireBaseQuestions = Record<
+  string,
+  {
+    author: {
+      name: string;
+      avatar: string;
+    };
+    content: string;
+    isAnswered: boolean;
+    isHighlighted: boolean;
+  }
+>;
+
+type Question = {
+  id: string;
+  author: {
+    name: string;
+    avatar: string;
+  };
+  content: string;
+  isAnswered: boolean;
+  isHighlighted: boolean;
+};
+
 export function Room() {
+  const { user } = useAuth();
+
   const params = useParams<RoomParams>();
+  const [newQuestion, setNewQuestion] = useState("");
+  const [questions, setQuestions] = useState<Question[]>([]);
+  const [title, setTitle] = useState("");
+  const roomId = params.id;
+
+  useEffect(() => {
+    //acessa as perguntas registradas na base da sala com o ID acessada pelo usuario
+    const roomRef = database.ref(`rooms/${roomId}`);
+    //dispara o event buscar os dados na base toda vez que houver uma mudança na base (inclusão de pergunta)
+    roomRef.on("value", (room) => {
+      const databaseRoom = room.val();
+      const firebaseQuestions: FireBaseQuestions = databaseRoom.questions;
+
+      const parsedQuestions = Object.entries(firebaseQuestions).map(
+        ([key, value]) => {
+          return {
+            id: key,
+            content: value.content,
+            author: value.author,
+            isHighlighted: value.isHighlighted,
+            isAnswered: value.isAnswered,
+          };
+        }
+      );
+
+      setTitle(databaseRoom.title);
+      setQuestions(parsedQuestions);
+    });
+  }, [roomId]);
+
+  async function handleSendQuestion(event: FormEvent) {
+    event.preventDefault();
+
+    if (newQuestion.trim() === "") return;
+
+    if (!user) throw new Error("You must be logged!");
+
+    const question = {
+      content: newQuestion,
+      author: {
+        name: user.name,
+        avatar: user.avatar,
+      },
+      isHighlighted: false,
+      isAnswered: false,
+    };
+
+    await database.ref(`rooms/${roomId}/questions`).push(question);
+    setNewQuestion("");
+  }
 
   return (
     <div id="page-room">
       <header>
         <div className="content">
           <img src={logoImg} alt="letmeask" />
-          <RoomCode code={params.id} />
+          <RoomCode code={roomId} />
         </div>
       </header>
 
       <main className="content">
         <div className="room-title">
-          <h1>Sala React</h1>
-          <span>4 perguntas</span>
+          <h1>Sala {title}</h1>
+          {questions.length > 0 && <span>{questions.length} pergunta(s)</span>}
         </div>
 
-        <form>
-          <textarea placeholder="O que você quer perguntar?" />
+        <form onSubmit={handleSendQuestion}>
+          <textarea
+            placeholder="O que você quer perguntar?"
+            value={newQuestion}
+            onChange={(event) => setNewQuestion(event.target.value)}
+          />
           <div className="form-footer">
-            <span>
-              Para enviar uma pergunta, <button>faça seu login</button>.
-            </span>
+            {user ? (
+              <div className="user-info">
+                <img src={user.avatar} alt="" />
+                <span>{user.name}</span>
+              </div>
+            ) : (
+              <span>
+                Para enviar uma pergunta, <button>faça seu login</button>.
+              </span>
+            )}
 
             <Button type="submit">Enviar pergunta</Button>
           </div>
