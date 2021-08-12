330521360074837131426ef9a20f573c5d5de0e3
Fernando Ferreira <fersants.nando.ferreira@gmail.com>
2021-08-11 23:36:23 -0300

    Refatorado o componente. Montar a lógica.

--- carousel-with-iframe/src/App.js
+++ carousel-with-iframe/src/App.js
@@ -2,7 +2,7 @@ import { Tree, Container, Content, Sidebar, Sidenav } from "rsuite";
 import data from "./data";
 import "rsuite/dist/styles/rsuite-default.css";
 import style from "./style.module.css";
-import { LeftArrow, RightArrow } from "./Arrows/Arrows.js";
+import Arrows from "./Arrows/Arrows.js";
 import { useState } from "react";
 function App() {
   const [pdfLinkToRender, setPdfLinkdToRender] = useState(
@@ -27,13 +27,11 @@ function App() {
       </div>
 
       <div className={style.container}>
-        <LeftArrow />
-
-        <div className={style.holdsIframe}>
-          <iframe src={pdfLinkToRender} width="600px"></iframe>
-        </div>
-
-        <RightArrow />
+        <Arrows setContentToBeShow={setPdfLinkdToRender} list={data}>
+          <div className={style.holdsIframe}>
+            <iframe src={pdfLinkToRender} width="600px"></iframe>
+          </div>
+        </Arrows>
       </div>
     </div>
   );
